fecd5730a5d1aebfa8995e2c4609a7789ac84e89
Grey Wind <wjunqin7@gmail.com>
2025-01-24 01:51:55 +0800

    feat: enable https (#3)
    * feat: 初始化项目
    
    * feat: use http
    
    * docs: update author
    
    * feat: enable https

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-// 引入所需的模块
 const http = require('http');
+const https = require('https');
 const httpProxy = require('http-proxy');
 
 // 创建代理服务器
 const proxy = httpProxy.createProxyServer({});
 
-// 创建服务器来监听请求
+// 创建一个 HTTP 服务器来监听请求
 const server = http.createServer((req, res) => {
-  // 获取目标 URL，例如 "www.myproxy.com/youtube.com" 中的 "youtube.com"
-  const targetUrl = req.url.split('/')[1]; // 从 URL 中提取目标域名
+  // 获取请求的目标 URL，例如 "www.myproxy.com/youtube.com"
+  const targetUrl = req.url.split('/')[1];  // 从 URL 中提取目标域名
 
-  // 将请求代理到目标 URL
-  const target = `http://${targetUrl}`;
+  // 选择使用 http 或 https
+  const protocol = targetUrl.startsWith('https') ? 'https' : 'http';
+  const target = `${protocol}://${targetUrl}`;
+
+  // 输出日志，查看代理请求的目标
+  console.log(`代理请求目标: ${target}`);
 
   // 处理代理请求
   proxy.web(req, res, { target: target }, (e) => {
@@ -21,7 +25,7 @@ const server = http.createServer((req, res) => {
   });
 });
 
-// 监听端口 8080
+// 启动代理服务器监听 HTTP 请求
 server.listen(8080, () => {
   console.log('代理服务器运行在 http://localhost:8080');
 });
